2be7f9c82826bd440f79b5d6e17a63a5fd2493b0
Hardi <hardivirani2003@gmail.com>
2024-11-30 18:15:11 +0530

    First

--- src/avg.java
+++ src/avg.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class avg {
+public class avg
+{
+    public static void main(String[]args)
+    {
+        Scanner object = new Scanner(System.in);
+
+        System.out.print("Enter your 1st number: ");
+        int a = object.nextInt();
+
+        System.out.print("Enter your 2nd number: " );
+        int b = object.nextInt();
+
+        System.out.print("Enter your 3rd number: ");
+        double c = object.nextDouble();
+
+        double avg = (a+b+c)/3;
+        System.out.print("Average: ");
+        System.out.print(avg);
+
+
+    }
+
 }
