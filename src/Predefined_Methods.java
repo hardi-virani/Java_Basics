944d6474bbb5868e71219fb1fd7a3ff10e3ee735
Hardi <hardivirani2003@gmail.com>
2024-12-07 09:22:33 +0530

    Clear Answers

--- src/Predefined_Methods.java
+++ src/Predefined_Methods.java
@@ -11,6 +11,15 @@ public class Predefined_Methods
         while(power<=32)
         {
             double ans = Math.pow(2,power);
+
+//            System.out.println(ans);
+
+            //2^%.0f this is the first part before = which means 2^3 : 3 being the power.
+            // second part after = is %0.f means the ans after the calculation means 8
+            //%n means next line.
+            //Always remember this expression as it will show clear answers.
+            // Ps. Do this practice more often to remember certain terms like this.
+            // COMPARE the upper code where we only print ans and then the below code. It shows the vast difference.
             System.out.printf("2^%.0f=%.0f%n", power,ans);
             power++;
         }
