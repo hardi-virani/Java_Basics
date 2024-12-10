a37eb67be27cd84d164cc8f3cc1524703a565ee9
Hardi <hardivirani2003@gmail.com>
2024-12-10 13:39:42 +0530

    Arrays

---
+++ src/Arrays_basic.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Arrays_basic
+{
+    public static void main(String[]args)
+    {
+        double[] temperatures = new double[64];
+
+        for (int i = 0; i < temperatures.length; i++)
+        {
+            temperatures[i] = 50;
+            System.out.println(i);
+        }
+    }
+}
